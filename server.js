ac0b08647a39812425fcd3ee6bf1feafe23faff5
Quiahn <quiahnb@gmail.com>
2021-09-21 01:07:34 -0400

    Change cors to accept env

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const app = express()
 app.use(fileUpload());
 // set CORS headers on response from this API using the `cors` NPM package
 // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
-app.use(cors({ origin: `http://localhost:${clientDevPort}` || process.env.CLIENT_ORIGIN }))
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${clientDevPort}` }))
 
 // define port for API to run on
 const port = process.env.PORT || serverDevPort
